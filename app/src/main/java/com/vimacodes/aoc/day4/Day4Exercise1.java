73b124704d82c1b380fcd11863cf4b444568000f
majerv <viktor.majer1@gmail.com>
2023-12-04 06:23:10 +0100

    day4/1 solved

--- app/src/main/java/com/vimacodes/aoc/day4/Day4Exercise1.java
+++ app/src/main/java/com/vimacodes/aoc/day4/Day4Exercise1.java
@@ -1,12 +1,12 @@
 package com.vimacodes.aoc.day4;
 
-
 class Day4Exercise1 {
 
   public long solve(final String text) {
     return text.lines()
-            .mapToInt(s -> 0)
-            .peek(v -> System.out.println("value: " + v))
-            .sum();
+        .map(Card::parse)
+        .peek(card -> System.out.println("card: " + card))
+        .mapToInt(Card::getPoints)
+        .sum();
   }
 }
