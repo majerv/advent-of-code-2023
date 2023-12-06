227b878b3043fa67f97e7f27c9d224f8601f4397
majerv <viktor.majer1@gmail.com>
2023-12-06 06:52:13 +0100

    day6 small refactor

--- app/src/main/java/com/vimacodes/aoc/day6/Races.java
+++ app/src/main/java/com/vimacodes/aoc/day6/Races.java
@@ -9,16 +9,8 @@ class Races {
   public static List<Race> parse(String text) {
     List<String> data = text.lines().toList();
 
-    List<Long> times =
-        Arrays.stream(data.get(0).split("Time:")[1].stripLeading().stripTrailing().split("\\s+"))
-            .map(Long::parseLong)
-            .toList();
-
-    List<Long> distances =
-        Arrays.stream(
-                data.get(1).split("Distance:")[1].stripLeading().stripTrailing().split("\\s+"))
-            .map(Long::parseLong)
-            .toList();
+    List<Long> times = parseLongs(data.get(0).split("Time:")[1]);
+    List<Long> distances = parseLongs(data.get(1).split("Distance:")[1]);
 
     List<Race> races = new ArrayList<>();
     for (int i = 0; i < times.size(); i++) {
@@ -26,5 +18,11 @@ class Races {
     }
 
     return races;
+  }
+
+  private static List<Long> parseLongs(String line) {
+    return Arrays.stream(line.stripLeading().stripTrailing().split("\\s+"))
+        .map(Long::parseLong)
+        .toList();
   }
 }
