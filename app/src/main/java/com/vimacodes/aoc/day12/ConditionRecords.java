226d386127cab116ed48cc9a31cbe9b6e1bd72e4
majerv <viktor.majer1@gmail.com>
2023-12-12 20:55:54 +0100

    day12/2 solved

--- app/src/main/java/com/vimacodes/aoc/day12/ConditionRecords.java
+++ app/src/main/java/com/vimacodes/aoc/day12/ConditionRecords.java
@@ -1,11 +1,12 @@
 package com.vimacodes.aoc.day12;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import lombok.Value;
 
 @Value
 class ConditionRecords {
+  private static final int DEFAULT_UNFOLD_FACTOR = 5;
+
   String condition;
   List<Integer> errorGroups;
 
@@ -15,11 +16,15 @@ class ConditionRecords {
         parts[0], Arrays.stream(parts[1].split(",")).map(Integer::parseInt).toList());
   }
 
-  public int arrangements() {
-    return arrangements(condition, errorGroups);
+  public long arrangements() {
+    Map<ConditionRecords, Long> memo = new HashMap<>();
+    return arrangements(this, memo);
   }
 
-  private int arrangements(String condition, List<Integer> errorGroups) {
+  private static long arrangements(ConditionRecords records, Map<ConditionRecords, Long> memo) {
+    String condition = records.getCondition();
+    List<Integer> errorGroups = records.getErrorGroups();
+
     if (condition.isEmpty()) {
       return errorGroups.isEmpty() ? 1 : 0;
     }
@@ -28,10 +33,16 @@ class ConditionRecords {
       return condition.contains("#") ? 0 : 1;
     }
 
-    int arrangements = 0;
+    if (memo.containsKey(records)) {
+      return memo.get(records);
+    }
+
+    long arrangements = 0;
     char c = condition.charAt(0);
 
-    if (c == '.' || c == '?') arrangements += arrangements(condition.substring(1), errorGroups);
+    if (c == '.' || c == '?') {
+      arrangements += arrangements(new ConditionRecords(condition.substring(1), errorGroups), memo);
+    }
 
     if (c == '#' || c == '?') {
       Integer num = errorGroups.get(0);
@@ -40,14 +51,37 @@ class ConditionRecords {
           && (num == condition.length() || condition.charAt(num) != '#')) {
         arrangements +=
             arrangements(
-                substrFrom(condition, num + 1), errorGroups.subList(1, errorGroups.size()));
+                new ConditionRecords(
+                    substrFrom(condition, num + 1), errorGroups.subList(1, errorGroups.size())),
+                memo);
       }
     }
 
+    memo.put(records, arrangements);
     return arrangements;
   }
 
-  private String substrFrom(String condition, int index) {
+  private static String substrFrom(String condition, int index) {
     return index >= condition.length() ? "" : condition.substring(index);
+  }
+
+  public ConditionRecords unfold() {
+    return unfold(DEFAULT_UNFOLD_FACTOR);
+  }
+
+  public ConditionRecords unfold(final int factor) {
+    StringBuilder newCondition = new StringBuilder();
+    List<Integer> newErrorGroups = new ArrayList<>();
+
+    for (int i = 0; i < factor; i++) {
+      newCondition.append(condition);
+      if (i != factor - 1) {
+        newCondition.append("?");
+      }
+
+      newErrorGroups.addAll(errorGroups);
+    }
+
+    return new ConditionRecords(newCondition.toString(), newErrorGroups);
   }
 }
--- app/src/main/java/com/vimacodes/aoc/day12/Day12Exercise2.java
+++ app/src/main/java/com/vimacodes/aoc/day12/Day12Exercise2.java
@@ -1,13 +1,12 @@
 package com.vimacodes.aoc.day12;
 
-class Day12Exercise1 {
+class Day12Exercise2 {
 
   public long solve(final String text) {
     return text.lines()
         .map(ConditionRecords::parse)
-        .peek(System.out::println)
+        .map(ConditionRecords::unfold)
         .mapToLong(ConditionRecords::arrangements)
-        .peek(System.out::println)
         .sum();
   }
 }
