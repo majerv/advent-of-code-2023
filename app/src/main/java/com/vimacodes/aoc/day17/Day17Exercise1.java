a75dc41afccb8f3e5b020ffd45dcf5950a2de876
majerv <viktor.majer1@gmail.com>
2023-12-16 08:08:12 +0100

    day17 prep

---
+++ app/src/main/java/com/vimacodes/aoc/day17/Day17Exercise1.java
@@ -0,0 +1,10 @@
+package com.vimacodes.aoc.day17;
+
+class Day17Exercise1 {
+  public long solve(final String text) {
+    //    Contraption contraption = Contraption.parse(text);
+    //    contraption.prettyPrint();
+
+    return 0;
+  }
+}
---
+++ app/src/test/java/com/vimacodes/aoc/day17/Day17Exercise1Test.java
@@ -0,0 +1,26 @@
+package com.vimacodes.aoc.day17;
+
+import com.vimacodes.aoc.Inputs;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class Day17Exercise1Test {
+  private final Day17Exercise1 exercise = new Day17Exercise1();
+
+  @ParameterizedTest
+  @MethodSource
+  void testInputs(final String inputName, long expectedResult) {
+    long result = exercise.solve(Inputs.resourceToString(inputName));
+    Assertions.assertEquals(expectedResult, result);
+  }
+
+  private static Stream<Arguments> testInputs() {
+    return Stream.of(
+        Arguments.of("day17_sample", 0), //
+        Arguments.of("day17_input", 0) //
+        );
+  }
+}
